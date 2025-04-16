d4109eee85cbbb1db5a29829eefa4fd4f064387d
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-16 11:18:01 +0700

    add1

--- test/NextDayCaculatorTest.java
+++ test/NextDayCaculatorTest.java
@@ -5,11 +5,11 @@ class NextDayCaculatorTest {
     @Test
     void testNextDay() {
         String result = NextDayCaculator.findNextDay(1,1,2018);
-
+        assertEquals("2/1/2018", result);
     }
     @Test
     void testNextDay2() {
         String result = NextDayCaculator.findNextDay(31,1,2018);
-
+        assertEquals("1/2/2018", result);
     }
 }
